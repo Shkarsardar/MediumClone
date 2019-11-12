e01245a945b11a156d35466690b834001aed202b
shkarsardar <shkarhsardar@gmail.com>
2019-11-12 17:32:57 +0300

    added splashscreen

---
+++ lib/screens/splashscreen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:medium/screens/homescreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (context)=>HomePage(),
+        ));
+
+    });
+
+
+  }
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold(
+      body: SafeArea(
+           child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,      
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Medium",style: TextStyle(fontWeight: FontWeight.w900,fontSize: 50,)),
+              SizedBox(height: 30,),
+              Image(image: AssetImage('lib/images/splash.png'),)
+
+            ],
+            ),
+        ),
+      ),
+    );
+
+  }
+}
