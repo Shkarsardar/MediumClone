9088caede88e38ae043f8601a6ec208005559035
shkarsardar <shkarhsardar@gmail.com>
2019-11-12 18:16:16 +0300

    added drawer and icons

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -6,12 +6,77 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey=new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      key: _scaffoldKey,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text("Drawer Header"),
+              decoration: BoxDecoration(
+                color: Colors.blue
+              ),
+            ),
+            ListTile(
+              title: Text("Your Account"),
+            )
+          ],
+        ),
+
+      ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.black,
+        title: Text("Home",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w700),),
+        titleSpacing: 5,
+        leading: Row(
+          children: <Widget>[
+            // SizedBox(width: 10),
+            IconButton(
+              splashColor: Colors.transparent,
+
+              icon: Icon(Icons.reorder),
+              onPressed: (){
+                _scaffoldKey.currentState.openDrawer();
+
+              },
+            )
+
+          ],
+        ),
+        actions: <Widget>[
+          
+          Row(
+
+            children: <Widget>[
+              IconButton(
+                splashColor: Colors.transparent,
+                icon: Icon(Icons.notifications_none,color: Colors.grey,),
+                onPressed: ()=>print("Notification"),
+
+              ),
+              
+              IconButton(
+                splashColor: Colors.transparent,
+                icon: Icon(Icons.search,color: Colors.grey),
+                onPressed: ()=>print("Search"),
+
+              ),
+              
+              
+
+              
+            ],
+          ),
+        ],
+      ),
       body: Text("Home Screen"),
-      
+
     );
   }
 }
